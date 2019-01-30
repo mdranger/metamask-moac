23f269d56cf0b5bbe6bf47b04a307138a8a34d04
mdranger <zhengpeng.li@moac.io>
2019-01-29 23:52:03 -0500

    added two additional files

---
+++ app/scripts/lib/buy-moac-url.js
@@ -0,0 +1,25 @@
+module.exports = getBuyMoacUrl
+
+/**
+ * Gives the caller a url at which the user can acquire MOAC, depending on the network they are in
+ *
+ * @param {object} opts Options required to determine the correct url
+ * @param {string} opts.network The network for which to return a url
+ * @param {string} opts.amount The amount of MOAC to buy on coinbene. Only relevant if network === '99'.
+ * @param {string} opts.address The address the bought MOAC should be sent to.  Only relevant if network === '99'.
+ * @returns {string|undefined} The url at which the user can access MOAC, while in the given network. If the passed
+ * network does not match any of the specified cases, or if no network is given, returns undefined.
+ *
+ */
+function getBuyMoacUrl ({ network, amount, address }) {
+  let url
+  switch (network) {
+    case '99':
+      url = 'https://www.coinbene.com/#/market?pairId=MOACUSDT'
+      break
+    case '101':
+      url = 'http://119.28.13.213:3000'
+      break
+  }
+  return url
+}
